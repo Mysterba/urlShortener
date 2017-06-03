0b83c283d51eff2d0ae6a6698b02dfa89d3261dd
Dennis <moh-dennis@hotmail.com>
2017-06-03 17:19:04 +0000

    Error handling

--- controllers/RedirectController.js
+++ controllers/RedirectController.js
@@ -12,16 +12,25 @@ function search(shortUrlNumber, callback) {
 		if (err) throw err
 		redirectUrl = documents;
 		callback();
-	});
+	})
 }
 
 exports.Redirect = function(request, response) {
     
     var shortUrlNumber = "https://boiling-brook-67511.herokuapp.com/"+request.url.substr(1);
-    console.log(shortUrlNumber)
+    console.log(shortUrlNumber);
 
     search(shortUrlNumber, function () {
-        response.redirect(redirectUrl[0].original_url);
+        
+        if (typeof redirectUrl[0] !== 'undefined') {
+            
+            response.redirect(redirectUrl[0].original_url);
+        } else {
+            var json = JSON.stringify({"error":"This url is not on the database."});
+            response.send(json);
+            response.end();
+        }
+        
     });
     
 };  
